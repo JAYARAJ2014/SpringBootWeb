e7d79ee407e89094385f2cf816fd086d252974e3
Jayaraj <jayaraj@pop-os.localdomain>
2025-09-01 11:42:10 -0400

    using ModelAndView. Commented the previous

--- src/main/java/com/jayinc/SpringBootWeb/HomeController.java
+++ src/main/java/com/jayinc/SpringBootWeb/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -47,17 +48,34 @@ public class HomeController {
 
 // Using model instead of session.
 
+//    @RequestMapping("add")
+//    public  String add(@RequestParam("num1") int num1 , @RequestParam("num2")int num2, Model model) {
+//        System.out.println("add() called...");
+//
+//        var result = num1 + num2;
+//
+//        model.addAttribute("num1", num1);
+//        model.addAttribute("num2", num2);
+//        model.addAttribute("result", result);
+//
+//        System.out.println(result);
+//        return "result";
+//    }
+
+
+//    Using ModelAndView object
     @RequestMapping("add")
-    public  String add(@RequestParam("num1") int num1 , @RequestParam("num2")int num2, Model model) {
+    public  ModelAndView add(@RequestParam("num1") int num1 , @RequestParam("num2")int num2, ModelAndView mv) {
         System.out.println("add() called...");
 
         var result = num1 + num2;
 
-        model.addAttribute("num1", num1);
-        model.addAttribute("num2", num2);
-        model.addAttribute("result", result);
+        mv.addObject("num1", num1);
+        mv.addObject("num2", num2);
+        mv.addObject("result", result);
+        mv.setViewName("result");
 
         System.out.println(result);
-        return "result";
+        return mv;
     }
 }
